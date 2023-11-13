488f80f49118951912f8b0ee89ebb96c3b8e97bb
Zendo27 <136874311+Zendo27@users.noreply.github.com>
2023-11-13 13:11:19 -0300

    la nota ya no sale de la pantalla

--- src/components/Nota.js
+++ src/components/Nota.js
@@ -1,12 +1,12 @@
-// Nota.jsx
 import React from "react";
 import { ListGroup, Button } from "react-bootstrap";
 
 export default function Nota({ id, titulo, descripcion, delete: deleteNote, notaProceso, notaTerminada}) {
   return (
-    <ListGroup.Item className="d-flex justify-content-between align-items-center">
+    <ListGroup.Item className="d-flex justify-content-between align-items-center"
+    style={{ maxHeight: "100px", overflow: "auto" }}>
       <div>
-        <strong>{titulo}</strong>: {descripcion}
+        <strong>{titulo}</strong><br></br> {descripcion}
       </div>
       <Button variant="danger" onClick={() => deleteNote(id)}>
         Eliminar
